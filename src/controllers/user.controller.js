9d8004471a5ffe409779ab1495f3dfa0403d73b5
kshitij-nandanwar <kshitijnandanwar0000@gmail.com>
2025-09-27 23:47:40 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,8 +6,18 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser=asyncHandler( async(req,res)=>{
     
-    const {fullname,email,username}=req.body
-    console.log("email: ",email);
+    // get user from frontend
+    //validation-not empty
+    //check if user already exists
+    //check for images, check for avatar
+    //upload them to cloudinary, avatar
+    //create user object-create entry in database
+    //remove password and refreshToken from response
+    //check if user is created successfully
+    //send response
+
+    const {fullname,email,username,password}=req.body
+    //console.log("email: ",email);
 
     if(
         [fullname,email,username].some((field)=>field?.trim()==="")
@@ -15,7 +25,7 @@ const registerUser=asyncHandler( async(req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or:[{username},{email}]
     })
     if(existedUser){
@@ -23,7 +33,12 @@ const registerUser=asyncHandler( async(req,res)=>{
     }
 
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath ){
         throw new ApiError(400,"Avatar is required")
